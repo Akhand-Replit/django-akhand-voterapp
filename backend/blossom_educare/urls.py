f0feacfc297e18657594d21c0b0633bb95db6a9a
Akhand-Replit <replit@akhandfoundation.com>
2025-09-26 18:43:16 +0600

    Update urls.py

--- backend/blossom_educare/urls.py
+++ backend/blossom_educare/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken import views as authtoken_views
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # Add this line to serve the index.html at the root
@@ -16,3 +18,8 @@ urlpatterns = [
     # This provides the standard DRF login/logout views for the browsable API
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
+
+# This is new: it tells Django to serve static files when DEBUG is False
+if not settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
