2201794a4960eadda7c91bf853e300ca03edd001
Akhand-Replit <replit@akhandfoundation.com>
2025-09-26 19:23:24 +0600

    Update urls.py

--- backend/blossom_educare/urls.py
+++ backend/blossom_educare/urls.py
@@ -1,25 +1,20 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework.authtoken import views as authtoken_views
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    # Add this line to serve the index.html at the root
-    path('', TemplateView.as_view(template_name='index.html'), name='home'),
-
     path('admin/', admin.site.urls),
     path('api/', include('records.urls')),
-
-    # This is the new, correct URL for our JavaScript frontend to get a token
     path('api/get-token/', authtoken_views.obtain_auth_token),
-
-    # This provides the standard DRF login/logout views for the browsable API
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
+    # This catch-all route will serve the frontend's index.html for any path not caught above.
+    re_path(r'^.*', TemplateView.as_view(template_name='index.html'), name='home'),
 ]
 
 # This is new: it tells Django to serve static files when DEBUG is False
 if not settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
